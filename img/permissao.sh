f086b2313c4f7c1b7275d9d0226d5d93048c7f52
Wesllen Matias Maia <wesllen.maia2010@gmail.com>
2020-05-14 11:00:59 -0300

    Alterado Dockerfile e adicionado script de permissoes

---
+++ img/permissao.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+chmod -R 777 /var/www/html/lhc_web/cache/ && \
+chmod 777 /var/www/html/lhc_web/settings/ && \
+chmod -R 777 /var/www/html/lhc_web/var/storage && \
+chmod -R 777 /var/www/html/lhc_web/var/userphoto && \
+chmod -R 777 /var/www/html/lhc_web/var/storageform && \
+chmod -R 777 /var/www/html/lhc_web/var/storageadmintheme && \
+chmod -R 777 /var/www/html/lhc_web/var/botphoto && \
+chmod -R 777 /var/www/html/lhc_web/var/bottrphoto && \
+chmod -R 777 /var/www/html/lhc_web/var/storageinvitation && \
+chmod -R 777 /var/www/html/lhc_web/var/storagedocshare && \
+chmod -R 777 /var/www/html/lhc_web/var/storagetheme && \
+chmod -R 777 /var/www/html/lhc_web/var/tmpfiles
